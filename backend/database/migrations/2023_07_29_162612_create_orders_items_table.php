0fd53476e0b80a1525331d972b9c34344a347cac
Husseini <mhmd.husseinyy@gmail.com>
2023-07-29 20:01:38 +0300

    migrating database

--- backend/database/migrations/2023_07_29_162612_create_orders_items_table.php
+++ backend/database/migrations/2023_07_29_162612_create_orders_items_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders_items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('address');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->unsignedBigInteger('item_id'); 
+            $table->unsignedBigInteger('order_id'); 
+            $table->unsignedBigInteger('quantity'); 
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders_items');
     }
 };
