369dfb61d8699f26770a2b6ad3036e273fd21aca
Husseini <mhmd.husseinyy@gmail.com>
2023-07-30 14:39:42 +0300

    creating register js function

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,32 +1,36 @@
-let signup_btn = document.getElementById('Signup-button');
-signup_btn.addEventListener('click', signup);
+document.addEventListener('DOMContentLoaded', function() {
+    let registrationForm = document.getElementById('registrationForm');
+    registrationForm.addEventListener('submit', signup);
+  });
 
-function signup() {
+function signup(event) {
+    event.preventDefault();
     let name = document.getElementById('Name').value;
     let email = document.getElementById('Email').value;
     let address = document.getElementById('Address').value;
     let password = document.getElementById('Password').value;
-    
+
     let data = new FormData();
     data.append('name', name);
     data.append('email', email);
     data.append('address', address);
     data.append('password', password);
-    
-    axios({
-        "method": "post",
-        "url": "http://127.0.0.1:8000/api/register",
-        "data": data
-    }).then((result) => {
-        alert("hi")
-        alert(result.data.authorisation.token)
-        console.log(result.data.authorisation.token)
-        if (result.data.status == "success") {
-            alert("signed up")
-            localStorage.setItem('token', result.data.authorisation.token) 
-            window.location.href = 'index.html'; 
-        }
-    }).catch((err) => {
-        console.error(err)
-    });
+
+    axios.post('http://127.0.0.1:8000/api/register', data)
+        .then((response) => {
+            console.log(response.data);
+            if (response.data.status === "success") {
+                alert("Signed up successfully!");
+                localStorage.setItem('token', response.data.authorisation.token);
+                // window.location.href = 'index.html';
+            } else {
+                alert("Registration failed. Please try again.");
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            alert("An error occurred during registration. Please try again later.");
+        });
+
+    return false; // Prevent the form from submitting normally
 }
